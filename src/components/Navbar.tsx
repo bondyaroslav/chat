38bec658a5b6ae2d671272e539893cfb519b9d0b
bondyaroslav <bondy2526@gmail.com>
2024-07-03 15:46:55 +0300

    Initial Commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+
+const Navbar = () => {
+    return (
+        <nav>
+            <Link to={'/'}>
+                <button>
+
+                </button>
+            </Link>
+        </nav>
+    )
+}
+
+export default Navbar
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {Provider} from "react-redux"
+import {BrowserRouter as Router} from "react-router-dom"
+import setupStore from "./redux/store"
+import App from './App'
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+initializeApp({
+    apiKey: "AIzaSyBK1--eoYYzqBF_0AJXehMlNZuJQ46AVSw",
+    authDomain: "chat-5f4d0.firebaseapp.com",
+    projectId: "chat-5f4d0",
+    storageBucket: "chat-5f4d0.appspot.com",
+    messagingSenderId: "442123115777",
+    appId: "1:442123115777:web:e45b3bbb610ec41521cf00",
+    measurementId: "G-X7D4PWBZW1"
+})
+
+const auth = getAuth()
+const firestore = getFirestore()
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    document.getElementById('root') as HTMLElement
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = setupStore()
+
+root.render(
+    <Provider store={store}>
+        <Router>
+            <App/>
+        </Router>
+    </Provider>
+)
